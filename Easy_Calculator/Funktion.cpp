965927f200d4dadce6ebd8f0e23b46b74d3aa10e
Christian Gnitka <c.gnitka@gmail.com>
2017-04-29 22:11:00 +0200

    issue#4 main completed

--- Easy_Calculator/Funktion.cpp
+++ Easy_Calculator/Funktion.cpp
@@ -3,7 +3,15 @@
 void addition() {
 	std::cout << "ich bin in der funktion " << std::endl;
 }
-void subtraktion();
-void Multiplikation();
-void Division();
-void output(int);
+void subtraktion() {
+
+}
+void Multiplikation() {
+
+}
+void Division() {
+
+}
+void output(int) {
+
+}
--- Easy_Calculator/main.cpp
+++ Easy_Calculator/main.cpp
@@ -4,25 +4,45 @@
 
 int main() {
 	char input;
+	
+
 	std::cout << "this is a easy Calculator" << std::endl
 		<< "A for Addition" << std::endl
 		<< "S for Subtraktion" << std::endl
 		<< "M for Multiplikation" << std::endl
-		<< "D for Division" << std::endl << std::endl
+		<< "D for Division" << std::endl 
+		<< "B for Break" << std::endl << std::endl
 		<< "Your input" << std::endl;
 	std::cin >> input;
 
 	switch (input)
 	{
 	case'A':
-	case'a': 
+	case'a':
 		std::cout << "ich bin im switch";
 		addition();
 		break;
+	case'S':
+	case's':
+		subtraktion();
+		break;
+	case'M':
+	case'm':
+		Multiplikation();
+		break;
+	case'D':
+	case'd':
+		Division();
+		break;
+	case'B':
+	case'b':
+		return 0;
 
 	default:
-		break;
+		std::cout << "Wrong input"<<std::endl;
+			break;
 	}
+	
 
 	system("pause");
 	return 0;
