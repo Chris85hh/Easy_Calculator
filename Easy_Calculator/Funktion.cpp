3aa82f129ac2321e65a4628e2b7419a76ccbf736
Christian Gnitka <c.gnitka@gmail.com>
2017-04-29 22:21:03 +0200

    issue #5 Programm Commplet

--- Easy_Calculator/Funktion.cpp
+++ Easy_Calculator/Funktion.cpp
@@ -1,17 +1,40 @@
 #include "Funktion.hpp"
 
 void addition() {
-	std::cout << "ich bin in der funktion " << std::endl;
+	int a, b;
+	std::cout << "number one" << std::endl;
+	std::cin >> a;
+	std::cout << "number two" << std::endl;
+	std::cin >> b;
+	output(a + b);
 }
 void subtraktion() {
+	int a, b;
+	std::cout << "number one" << std::endl;
+	std::cin >> a;
+	std::cout << "number two" << std::endl;
+	std::cin >> b;
+	output(a - b);
 
 }
 void Multiplikation() {
+	int a, b;
+	std::cout << "number one" << std::endl;
+	std::cin >> a;
+	std::cout << "number two" << std::endl;
+	std::cin >> b;
+	output(a * b);
 
 }
 void Division() {
+	int a, b;
+	std::cout << "number one" << std::endl;
+	std::cin >> a;
+	std::cout << "number two" << std::endl;
+	std::cin >> b;
+	output(a / b);
 
 }
-void output(int) {
-
+void output(int a) {
+	std::cout << "Results " << a << std::endl<<std::endl << std::endl << std::endl;
 }
--- Easy_Calculator/main.cpp
+++ Easy_Calculator/main.cpp
@@ -5,42 +5,45 @@
 int main() {
 	char input;
 	
-
-	std::cout << "this is a easy Calculator" << std::endl
-		<< "A for Addition" << std::endl
-		<< "S for Subtraktion" << std::endl
-		<< "M for Multiplikation" << std::endl
-		<< "D for Division" << std::endl 
-		<< "B for Break" << std::endl << std::endl
-		<< "Your input" << std::endl;
-	std::cin >> input;
-
-	switch (input)
+	while (true)
 	{
-	case'A':
-	case'a':
-		std::cout << "ich bin im switch";
-		addition();
-		break;
-	case'S':
-	case's':
-		subtraktion();
-		break;
-	case'M':
-	case'm':
-		Multiplikation();
-		break;
-	case'D':
-	case'd':
-		Division();
-		break;
-	case'B':
-	case'b':
-		return 0;
 
-	default:
-		std::cout << "Wrong input"<<std::endl;
+
+		std::cout << "this is a easy Calculator" << std::endl
+			<< "A for Addition" << std::endl
+			<< "S for Subtraktion" << std::endl
+			<< "M for Multiplikation" << std::endl
+			<< "D for Division" << std::endl
+			<< "B for Break" << std::endl << std::endl
+			<< "Your input" << std::endl;
+		std::cin >> input;
+
+		switch (input)
+		{
+		case'A':
+		case'a':
+			addition();
 			break;
+		case'S':
+		case's':
+			subtraktion();
+			break;
+		case'M':
+		case'm':
+			Multiplikation();
+			break;
+		case'D':
+		case'd':
+			Division();
+			break;
+		case'B':
+		case'b':
+			return 0;
+
+		default:
+			std::cout << "Wrong input" << std::endl;
+			break;
+		}
 	}
 	
 
